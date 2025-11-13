23ae05b8114a633fce154406df1a53f3caabebfb
Lucas Fernandes <lucasffb120@gmail.com>
2025-11-13 10:24:54 -0300

    feat(auth): módulo de autenticação com observador e testes funcionais

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { getApps } from 'firebase/app'; 
 import './src/services/firebase';
+import AuthTest from './src/tests/AuthTest';
 
 export default function App() {
   const apps = getApps();
 
   return (
     <View style={styles.container}>
-      <Text>{apps.length ? 'Firebase inicializado' : 'Firebase não inicializado'}</Text>
+      <AuthTest />
       <StatusBar style="auto" />
     </View>
   );
